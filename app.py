b82d6b9d9f9833aabc32dabebcf72294fcd49945
Alexsoto151006 <20243rd037@utez.edu.mx>
2025-11-05 16:24:23 -0600

    Add files via upload
    Alexander Soto 
    EStroy agrgando el ultimo metodo de mi codigo que seria put

--- app.py
+++ app.py
@@ -54,6 +54,35 @@ def mostrar_dispositivos_html():
     """
     return html
 
+@app.route('/dispositivos', methods=['POST'])
+def agregar_dispositivo():
+    nuevo = request.get_json()
+    dispositivos[nuevo['id']] = {
+        'nombre': nuevo['nombre'],
+        'descripcion': nuevo['descripcion'],
+        'ip': nuevo['ip'],
+        'mac': nuevo['mac'],
+        'ubicacion': nuevo['ubicacion'],
+        'tipo': nuevo['tipo'],
+        'otros': nuevo.get('otros', '')
+    }
+    return {"mensaje": "Dispositivo agregado correctamente"}, 201
+
+@app.route('/dispositivos/<id>', methods=['PUT'])
+def modificar_dispositivo(id):
+    if id not in dispositivos:
+        return {"error": "No encontrado"}, 404
+    datos = request.get_json()
+    dispositivos[id].update({
+        'nombre': datos.get('nombre', dispositivos[id]['nombre']),
+        'descripcion': datos.get('descripcion', dispositivos[id]['descripcion']),
+        'ip': datos.get('ip', dispositivos[id]['ip']),
+        'mac': datos.get('mac', dispositivos[id]['mac']),
+        'ubicacion': datos.get('ubicacion', dispositivos[id]['ubicacion']),
+        'tipo': datos.get('tipo', dispositivos[id]['tipo']),
+        'otros': datos.get('otros', dispositivos[id]['otros'])
+    })
+    return {"mensaje": "Dispositivo actualizado correctamente"}
 
 if __name__ == '__main__':
     app.run(debug=True)
